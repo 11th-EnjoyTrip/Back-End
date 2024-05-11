73f2d6c27916351b3d472f6b7a868f78ba2dd425
KimJeongIn <kimjeongin@KimJeongInui-MacBookAir.local>
2024-05-11 19:40:38 +0900

    Fix : Read, member info

--- travelog/src/main/java/com/travelog/member/dao/MemberDao.java
+++ travelog/src/main/java/com/travelog/member/dao/MemberDao.java
@@ -4,10 +4,15 @@ import com.travelog.member.dto.MemberDto;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.sql.SQLException;
 import java.util.List;
 
 @Mapper
 public interface MemberDao {
-    @Select("SELECT * FROM member")
-    List<MemberDto> getMembers();
+    @Select(value = "SELECT * FROM member")
+    List<MemberDto> getMembers() throws SQLException;
+
+    @Select(value = "SELECT * FROM member WHERE id = #{memberId}")
+    public MemberDto memberInfo(String memberId) throws SQLException;
+
 }
--- travelog/src/main/java/com/travelog/member/service/MemberService.java
+++ travelog/src/main/java/com/travelog/member/service/MemberService.java
@@ -2,8 +2,11 @@ package com.travelog.member.service;
 
 import com.travelog.member.dto.MemberDto;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface MemberService {
-    List<MemberDto> getMembers();
+    List<MemberDto> getMembers() throws Exception;
+    public MemberDto memberInfo(String memberId) throws SQLException;
+
 }
