16b55b54677313a3ca1e539a00063e4df1356a84
nkyungm <skarudals27@naver.com>
2024-05-14 17:58:21 +0900

    feat-#9-search-attraction - 관광지 관련 api 구현

--- travelog/src/main/java/com/travelog/attraction/controller/AttractionController.java
+++ travelog/src/main/java/com/travelog/attraction/controller/AttractionController.java
@@ -5,27 +5,33 @@ import com.travelog.attraction.service.AttractionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/attraction")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class AttractionController {
     @Autowired
     AttractionService attractionService;
 
     @GetMapping("")
-    public ResponseEntity<?> getAttractionList(@RequestBody AttractionRequestDto attractionRequestDto, @PageableDefault(size=20) Pageable pageable) throws Exception{
-        return ResponseEntity.ok(attractionService.getAttractionList(attractionRequestDto, pageable)) ;
-    }
-
-    @GetMapping("/test")
-    public int getTest(){
-        return 1;
+    public ResponseEntity<?> getAttractionList(@RequestBody AttractionRequestDto attractionRequestDto, @PageableDefault(size=20) Pageable pageable) {
+        try {
+            return ResponseEntity.ok(attractionService.getAttractionList(attractionRequestDto, pageable).getContent()) ;
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("SERVER ERROR");
+        }
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getAttractionDetail(@PathVariable("id") String id) throws Exception{
-        return ResponseEntity.ok(attractionService.getAttractionDetail(Integer.parseInt(id)));
+    public ResponseEntity<?> getAttractionDetail(@PathVariable("id") int id) {
+        try {
+            return ResponseEntity.ok(attractionService.getAttractionDetail(id));
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("SERVER ERROR");
+        }
     }
 }
--- travelog/src/main/java/com/travelog/attraction/dao/AttractionDao.java
+++ travelog/src/main/java/com/travelog/attraction/dao/AttractionDao.java
@@ -11,54 +11,52 @@ import java.util.Map;
 @Mapper
 public interface AttractionDao {
 
-    @Select("SELECT" +
-            "    content_id,content_type_id," +
-            "   CASE " +
-            "        WHEN content_type_id = 12 THEN '관광지'" +
-            "        WHEN content_type_id = 14 THEN '문화시설'" +
-            "        WHEN content_type_id = 15 THEN '축제/공연/행사'" +
-            "        WHEN content_type_id = 28 THEN '레포츠'" +
-            "        WHEN content_type_id = 32 THEN '숙박'" +
-            "        WHEN content_type_id = 38 THEN '쇼핑'" +
-            "        WHEN content_type_id = 39 THEN '음식'" +
-            "        ELSE '기타'" +
-            "    END AS content_type_name,title,first_image,ai.sido_code,s.sido_name,latitude,longitude " +
-            "FROM attraction_info ai JOIN sido s ON ai.sido_code = s.sido_code " +
-            "WHERE" +
-            "    ai.sido_code = IF(#{data.region} = 0,ai.sido_code,#{data.region})" +
-            "    AND  FIND_IN_SET(content_type_id, #{data.category})" +
-            "    AND title LIKE CONCAT('%', #{data.keyword}, '%')"+
+    @Select("SELECT " +
+            "    content_id AS contentId, content_type_id AS contentTypeId, " +
+            "    CASE " +
+            "        WHEN content_type_id = 12 THEN '관광지' " +
+            "        WHEN content_type_id = 14 THEN '문화시설' " +
+            "        WHEN content_type_id = 15 THEN '축제/공연/행사' " +
+            "        WHEN content_type_id = 28 THEN '레포츠' " +
+            "        WHEN content_type_id = 32 THEN '숙박' " +
+            "        WHEN content_type_id = 38 THEN '쇼핑' " +
+            "        WHEN content_type_id = 39 THEN '음식' " +
+            "        ELSE '기타' " +
+            "    END AS contentTypeName, title, first_image AS firstImage, " +
+            "    ai.sido_code AS sidoCode, s.sido_name AS sidoName " +
+            "FROM attraction_info ai " +
+            "JOIN sido s ON ai.sido_code = s.sido_code " +
+            "WHERE " +
+            "    ai.sido_code = IF(#{data.region} = 0, ai.sido_code, #{data.region}) " +
+            "    AND FIND_IN_SET(content_type_id, #{data.category}) " +
+            "    AND title LIKE CONCAT('%', #{data.keyword}, '%') " +
             "LIMIT #{pageable.pageSize} OFFSET #{pageable.offset}")
     List<Map<String,Object>> getAttractionList(RequestList<?> requestList) throws SQLException;
 
-    @Select("select ai.content_id, content_type_id," +
-            "case " +
-            "when content_type_id = 12 then '관광지'" +
-            "    when content_type_id = 14 then '문화시설'" +
-            "    when content_type_id = 15 then '축제/공연/행사'" +
-            "    when content_type_id = 28 then '레포츠'" +
-            "    when content_type_id = 32 then '숙박'" +
-            "    when content_type_id = 38 then '쇼핑'" +
-            "    when content_type_id = 39 then '음식'" +
-            "else '기타' end as content_type_name" +
-            ",title,addr1,addr2,zipcode,tel,first_image," +
-            "ai.sido_code,s.sido_name,latitude,longitude,ad.overview" +
-            "from attraction_info ai join sido s on ai.sido_code = s.sido_code" +
-            "join attraction_description ad on ai.content_id = ad.content_id" +
-            "where ai.content_id = #{id}")
+    @Select("SELECT ai.content_id, content_type_id," +
+            "CASE " +
+            "    WHEN content_type_id = 12 THEN '관광지' " +
+            "    WHEN content_type_id = 14 THEN '문화시설' " +
+            "    WHEN content_type_id = 15 THEN '축제/공연/행사' " +
+            "    WHEN content_type_id = 28 THEN '레포츠' " +
+            "    WHEN content_type_id = 32 THEN '숙박' " +
+            "    WHEN content_type_id = 38 THEN '쇼핑' " +
+            "    WHEN content_type_id = 39 THEN '음식' " +
+            "    ELSE '기타' " +
+            "END AS content_type_name, " +
+            "title, addr1, addr2, zipcode, tel, first_image, " +
+            "ai.sido_code, s.sido_name, latitude, longitude, ad.overview " +
+            "FROM attraction_info ai " +
+            "JOIN sido s ON ai.sido_code = s.sido_code " +
+            "JOIN attraction_description ad ON ai.content_id = ad.content_id " +
+            "WHERE ai.content_id = #{id}")
+    @Result(property = "contentId", column = "content_id")
+    @Result(property = "contentTypeId",column = "content_type_id")
+    @Result(property = "contentTypeName",column = "content_type_name")
+    @Result(property = "firstImage",column = "first_image")
+    @Result(property = "sidoCode",column = "sido_code")
+    @Result(property = "sidoName",column = "sido_name")
     AttractionDetailDto getAttractionDetail(int id) throws SQLException;
 
-    // " OFFSET #{pageable.offset} ROWS FETCH NEXT #{pageable.pageSize} ROWS ONLY")
-    // Select ArticleList
-//    @Select(value = "select * from board order by id")
-//    @Results(id = "list", value = {
-//            @Result(property = "id", column = "id"),
-//            @Result(property = "writer", column = "writer"),
-//            @Result(property = "title", column = "title"),
-//            @Result(property = "content", column = "content"),
-//            @Result(property = "view_cnt", column = "view_cnt"),
-//            @Result(property = "reg_data", column = "reg_data"),
-//    })
-//    List<BoardDto> listArticle() throws SQLException;
 
 }
